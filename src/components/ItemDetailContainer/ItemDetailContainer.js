8a0dbace331d17fc209fb55e8e902ca58f0ca08f
Daniel Rozitchner <danirozitchner@gmail.com>
2021-12-12 22:36:38 -0300

    item dinamico

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -16,38 +16,10 @@ const ItemDetailContainer = () => {
     
     
     })
-    console.log(id);
-    console.log(item);
     return (
       <div>
         <ItemDetail item={item} /> 
         </div> 
-      )
-      //   getOneProduct.then((res) => setItem(res)).catch((err) => console.log(err));
-      // });
-      // return (
-      //   <div>
-      //     <ItemDetail item={item} />
-      //   </div>
-      // );
-       /* // key={item.id}
-          //   className='col-md-4'
-          //                               >
-          //       <div className="card w-100 mt-5" >
-          //           <div className="card-header">
-          //               {`${item.cake}`}
-          //           </div>
-          //           <div className="card-body">
-          //               <img src={item.foto} alt='' className='w-50' />
-          //                   {item.detalle}
-          //                   {`$${item.precio}`}
-          //           </div>
-          //           <div className="card-footer">
-          //           <button className="btn btn-outline-primary btn-block">
-          //             Agregar al Carrito
-          //           </button>
-                    
-          //           </div>
-          //       </div> */
+      )  
   }
 export default ItemDetailContainer
