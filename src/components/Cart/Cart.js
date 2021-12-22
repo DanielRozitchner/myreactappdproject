2cf0899812aefb973f309f3f96fdd090b482586b
Daniel Rozitchner <danirozitchner@gmail.com>
2021-12-21 23:45:47 -0300

    na fotku

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,7 +6,7 @@ function Cart() {
     const { cartList, borrarCarrito } = useCartContext()
     return (
         <div>
-            { cartList.map(prod => <div>{prod.cake} {prod.cantidad} {`$${prod.precio}`}</div>)} 
+            { cartList.map(prod => <div><img src={prod.foto} alt='' className='img-fluid' width= '10%' />{prod.cake}  {prod.cantidad} {`$${prod.precio}`}</div>)} 
             <button onClick={borrarCarrito}>Vaciar carrito </button>
         </div>
     )
